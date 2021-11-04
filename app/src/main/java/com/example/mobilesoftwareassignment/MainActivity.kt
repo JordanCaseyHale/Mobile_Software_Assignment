a009563a126ecec0f6540b79ec313689964a42b6
Chelsie <coedun1@sheffield.ac.uk>
2021-11-04 12:11:14 +0000

    Test comment added to MainActivity.kt

--- app/src/main/java/com/example/mobilesoftwareassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobilesoftwareassignment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mobilesoftwareassignment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Will add comments here
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
