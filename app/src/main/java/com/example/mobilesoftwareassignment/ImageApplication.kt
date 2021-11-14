33fdb67eec9f3658f5ec9005453119371abe4a70
A-Alaziz <classicalbloshi@gmail.com>
2021-11-14 18:52:38 +0000

    test push

--- app/src/main/java/com/example/mobilesoftwareassignment/ImageApplication.kt
+++ app/src/main/java/com/example/mobilesoftwareassignment/ImageApplication.kt
@@ -3,6 +3,7 @@ package com.example.mobilesoftwareassignment
 import android.app.Application
 import com.example.mobilesoftwareassignment.data.ImageRoomDatabase
 
+// test push
 class ImageApplication: Application() {
     val databaseObj: ImageRoomDatabase by lazy { ImageRoomDatabase.getDatabase(this) }
 }
