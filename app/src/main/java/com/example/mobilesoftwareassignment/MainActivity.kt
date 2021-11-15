aed43ee94b66bbcb34e4f28d2bb23ff71add93c9
Jordan Hale <jordancaseyhale@gmail.com>
2021-11-15 15:51:05 +0000

    Current tests

--- app/src/main/java/com/example/mobilesoftwareassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobilesoftwareassignment/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.mobilesoftwareassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.mobilesoftwareassignment.databinding.ActivityMainBinding
 
 // Will add comments here
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
     }
 }
